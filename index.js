73017f27912543393a46da6c4b04b95338cdc94b
BenChen0824 <ben216033@gmail.com>
2022-06-20 13:33:16 +0800

    get_post npm(cors)

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const app = express()
 const db = require('./connect_db');
 
 
+var cors = require('cors');
+
+app.use(express.urlencoded())
+app.use(cors())
+
 app.get('/', function (req, res) {
   res.send('Hello World12345')
 })
 
         //這邊 /後面的意思是說 在主伺服器後面增加路徑 導向頁面
 app.get('/cart', async (req, res) =>{
-    const sql = 'SELECT * FROM `orderlist`';
-    [rs]=await db.query(sql);
-    res.json(rs);
-    
+    const sql = 'SELECT * FROM `orderlist` ';
+    const [rs]=await db.query(sql);
+    res.json({'db':rs,"session":true});
+})
+
+app.post('/cart', async (req, res) => {
+    console.log(req.body);
+    const sql = 'SELECT * FROM `orderlist` WHERE customer_id = ?';
+    const [rs]=await db.query(sql,[req.body.sid]);
+    res.json({'db':rs,"session":true});
     
 })
 app.get('/member', function (req, res) {
