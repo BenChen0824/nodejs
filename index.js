4f3224da7b5b97fedae935b875d3454655fc3a0b
BenChen0824 <ben216033@gmail.com>
2022-06-20 11:01:18 +0800

    connect pool export

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 const express = require('express')
 const app = express()
 // get the client
-const mysql = require('mysql2');
+const db = require('./connect_db');
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'ben',
-  password: "ben",
-  database: 'farmer26'
-});
 
 app.get('/', function (req, res) {
   res.send('Hello World12345')
 })
 
         //這邊 /後面的意思是說 在主伺服器後面增加路徑 導向頁面
-app.get('/cart_list', function (req, res) {
-    connection.query(
-        'SELECT * FROM `orderlist`',
-        function(err, results, fields) {
-          console.log(results); // results contains rows returned by server
-          //console.log(fields); // fields contains extra meta data about results, if available
-          res.json(results);
-        }
-    );
+app.get('/cart', async (req, res) =>{
+    const sql = 'SELECT * FROM `orderlist`';
+    [rs]=await db.query(sql);
+    res.json(rs);
     
     
 })
